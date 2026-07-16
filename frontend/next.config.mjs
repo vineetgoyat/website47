c5fc54b26b28c1f7e86ec0d445dbc7cdceaccf89
Vineet <vineetgoyat1@gmail.com>
2026-07-16 07:59:33 +0530

    website custom

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -3,6 +3,7 @@ const nextConfig = {
   images: {
     remotePatterns: [
       { protocol: "https", hostname: "images.unsplash.com" },
+      { protocol: "https", hostname: "ai47labs.com" },
     ],
   },
 };
